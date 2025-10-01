0b94a66e5ff1a5b47eafeb5b1089e88d4d1bb4b6
LEGOKOALA <144147004+LEGOKOALA@users.noreply.github.com>
2025-10-01 19:20:53 -0400

    update

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -1,11 +1,34 @@
-extends Node2D
+extends CharacterBody2D
 
+const SPEED = 60
+const GRAVITY = 400
+const MAX_FALL_SPEED = 300
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var direction = 1
 
+@onready var ray_cast_right: RayCast2D = $RayCastRight
+@onready var ray_cast_left: RayCast2D = $RayCastLeft
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _physics_process(delta: float) -> void:
+	# Gravity
+	velocity.y += GRAVITY * delta
+	velocity.y = min(velocity.y, MAX_FALL_SPEED)
+
+	# Wall detection
+	if ray_cast_right.is_colliding():
+		direction = -1
+		animated_sprite.flip_h = true
+		print("Right hit:", ray_cast_right.get_collider())
+	elif ray_cast_left.is_colliding():
+		direction = 1
+		animated_sprite.flip_h = false
+		print("Left hit:", ray_cast_left.get_collider())
+
+	# Only walk when grounded
+	if is_on_floor():
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = 0
+
+	move_and_slide()
